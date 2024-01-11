ae3ecff37143f740cb10901c0ab75dcb96f4cf14
Dzmitry <dedichpgu@gmail.com>
2024-01-11 20:17:55 +0300

    created a model of Mini Portfolio on the main and refactoring

--- webapp/models.py
+++ webapp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+# Подписчики
 class Subscriber(models.Model):
     email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.email
 
-
+# Прайс-планы главная страница
 class PricingPlan(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -18,9 +19,8 @@ class PricingPlan(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
 
-
+# Баннер на base.html
 class Banner(models.Model):
     title = models.CharField(max_length=100)
     description = RichTextField()
@@ -29,3 +29,11 @@ class Banner(models.Model):
 
     def __str__(self):
         return self.title
+
+# Портфолио мини на главную
+class Portfolio(models.Model):
+    image = models.ImageField(upload_to='portfolio_images/')
+    alt_text = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.alt_text or "Portfolio Item"
