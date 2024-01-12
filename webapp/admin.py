1ef062300a544214a019a9ddded24ff5d7a67515
Dzmitry <dedichpgu@gmail.com>
2024-01-12 23:12:12 +0300

    end created a model of Mini Portfolio  on the main and refactoring admin.py register

--- webapp/admin.py
+++ webapp/admin.py
@@ -5,26 +5,16 @@ from .models import *
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.db import models
 
-
 class MyModelAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': CKEditorUploadingWidget},
     }
 
-
+# Регистрация модели PricingPlan с использованием MyModelAdmin
 admin.site.register(PricingPlan, MyModelAdmin)
 
+# Регистрация модели Portfolio с использованием MyModelAdmin
+admin.site.register(Portfolio, MyModelAdmin)
 
-class BannerAdminForm(forms.ModelForm):
-    class Meta:
-        model = Banner
-        fields = '__all__'
-        widgets = {
-            'description': CKEditorWidget(),
-            'description_two': CKEditorWidget(),
-        }
-
-@admin.register(Banner)
-class BanerAdmin(admin.ModelAdmin):
-    form = BannerAdminForm
-    list_display = ('title', 'description', 'description_two', 'image')
+# Регистрация модели Portfolio с использованием MyModelAdmin
+admin.site.register(Banner, MyModelAdmin)
