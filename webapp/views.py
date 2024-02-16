d95a3021238467bb3cfb1e34eda2c73b40d690b6
Dzmitry <dedichpgu@gmail.com>
2024-02-16 18:08:37 +0300

    Added new social footer section

--- webapp/views.py
+++ webapp/views.py
@@ -1,46 +1,43 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
-from .forms import SubscribeForm
 from django.core.mail import send_mail
-from django.db import IntegrityError  # Импорт для обработки исключений
+from django.db import IntegrityError
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import SubscribeForm
 from .models import *
 from blog.models import BlogPost
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def send_confirmation_email(email):
-    # Функция для отправки подтверждающего письма
+    """
+    Функция для отправки подтверждающего письма подписчику.
+    """
     subject = 'Подтверждение подписки'
     message = 'Спасибо за подписку на нашу рассылку!'
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email, ]
+    recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
 
-
 def subscribe(request):
-    # Обработка запроса на подписку
+    """
+    Обработка запроса на подписку.
+    """
     if request.method == 'POST':
         form = SubscribeForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             try:
-                # Попытка создать новую запись подписчика
                 Subscriber.objects.create(email=email)
-                # Отправка подтверждающего письма
                 send_confirmation_email(email)
-                # Перенаправление на страницу успеха
-                return redirect('webapp:home')
+                return redirect('webapp:home')  # Перенаправление на страницу успеха
             except IntegrityError:
-                # Обработка случая, если email уже существует
                 print("Этот email уже подписан.")
     else:
         form = SubscribeForm()
-
     return render(request, 'main/base.html', {'form': form})
-
 
 def index(request):
     """
-    Renders the index page.
+    Отображение главной страницы.
     """
     site = Portfolio.objects.all()
     services = Service.objects.all()
@@ -49,32 +46,22 @@ def index(request):
     latest_posts = BlogPost.objects.order_by('-post_date')[:6]
     return render(request, 'webapp/index.html', {'site': site, 'developers': developers, 'services': services,'faqs': faqs, 'latest_posts': latest_posts})
 
-
-
-
 def about(request):
-
-
+    """
+    Отображение страницы "О нас".
+    """
     projects = PortfolioProject.objects.all()
-
-    # Разбиваем проекты на страницы (по 9 проектов на страницу)
     paginator = Paginator(projects, 9)
     page = request.GET.get('page', 1)
-
     try:
         portfolio = paginator.page(page)
     except PageNotAnInteger:
         portfolio = paginator.page(1)
     except EmptyPage:
         portfolio = paginator.page(paginator.num_pages)
-
-    # Создаем диапазон страниц для пагинации
     page_range = paginator.page_range
-
-    # Текущая страница
     current_page = int(page)
     return render(request, 'webapp/about.html', {'portfolio': portfolio, 'page_range': page_range, 'current_page': current_page})
-
 def services(request):
     """
     Renders the services page.
