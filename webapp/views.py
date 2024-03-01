3821bdbcbf38bd35814e8bda0c22ef85b01d3b6d
Dzmitry <dedichpgu@gmail.com>
2024-03-01 23:15:43 +0300

    switched views

--- webapp/views.py
+++ webapp/views.py
@@ -7,6 +7,7 @@ from .forms import SubscribeForm, ContactForm
 from .models import *
 from blog.models import BlogPost
 
+
 def send_confirmation_email(email):
     """
     Функция для отправки подтверждающего письма подписчику.
@@ -16,6 +17,7 @@ def send_confirmation_email(email):
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
+
 
 def subscribe(request):
     """
@@ -35,22 +37,27 @@ def subscribe(request):
         form = SubscribeForm()
     return render(request, 'main/base.html', {'form': form})
 
+
 def index(request):
     """
     Отображение главной страницы.
     """
     site = Portfolio.objects.all()
-    services = Service.objects.all()
+    services_index = Service.objects.all()
     developers = Developer.objects.all()
     faqs = FAQ.objects.all()
     latest_posts = BlogPost.objects.order_by('-post_date')[:6]
-    return render(request, 'webapp/index.html', {'site': site, 'developers': developers, 'services': services,'faqs': faqs, 'latest_posts': latest_posts})
+    return render(request, 'webapp/index.html',
+                  {'site': site, 'developers': developers, 'services': services_index, 'faqs': faqs,
+                   'latest_posts': latest_posts})
+
 
 def about(request):
     """
     Отображение страницы "О нас".
     """
     projects = PortfolioProject.objects.all()
+    review = Review.objects.all()
     paginator = Paginator(projects, 9)
     page = request.GET.get('page', 1)
     try:
@@ -61,18 +68,23 @@ def about(request):
         portfolio = paginator.page(paginator.num_pages)
     page_range = paginator.page_range
     current_page = int(page)
-    return render(request, 'webapp/about.html', {'portfolio': portfolio, 'page_range': page_range, 'current_page': current_page})
+    return render(request, 'webapp/about.html',
+                  {'portfolio': portfolio, 'page_range': page_range, 'current_page': current_page, 'review': review})
+
+
 def services(request):
     """
     Renders the services page.
     """
     return render(request, 'webapp/services.html')
 
+
 def faq(request):
     """
     Renders the FAQ page.
     """
     return render(request, 'webapp/faq.html')
+
 
 def team(request):
     """
@@ -81,12 +93,14 @@ def team(request):
     """
     return render(request, 'webapp/teams.html')
 
+
 def pars(request):
     """
     Renders the testimonials page.
     TODO: Add logic for testimonials page processing.
     """
     return render(request, 'webapp/testimonial.html')
+
 
 def bot(request):
     """
@@ -95,6 +109,7 @@ def bot(request):
     """
     return render(request, 'webapp/testimonial.html')
 
+
 def web_beck(request):
     """
     Renders the testimonials page.
@@ -102,12 +117,14 @@ def web_beck(request):
     """
     return render(request, 'webapp/testimonial.html')
 
+
 def pricing(request):
     """
     Renders the pricing page.
     TODO: Add logic for testimonials page processing.
     """
     return render(request, 'webapp/pricing.html')
+
 
 def contact(request):
     """
@@ -140,12 +157,9 @@ def contact(request):
 
     return render(request, 'webapp/contact.html', {'form': form})
 
+
 def portfolio_detail(request, project_id):
     # Получаем объект проекта по его ID или возвращаем 404, если проект не найден
     project = get_object_or_404(PortfolioProject, id=project_id)
 
     return render(request, 'webapp/portfolio_detail.html', {'project': project})
-
-
-
-
